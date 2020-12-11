39f411c1dfdb1e0705018c7c638294749c0b668a
supriti-github <75122616+supriti-github@users.noreply.github.com>
2020-12-12 00:43:08 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -3,7 +3,7 @@ class Box {
     constructor(x,y,width,height) {
 
         var options = {
-            restitution: 1.0
+            restitution: 0.8
         }
 
         this.body = Bodies.rectangle(x,y,width,height,options);
