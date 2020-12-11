e6fd3979bae97c4f47ff3fa757fcf54295581c12
supriti-github <75122616+supriti-github@users.noreply.github.com>
2020-12-11 23:58:33 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -21,6 +21,7 @@ class Box {
         translate(pos.x,pos.y);
         rotate(angle);
         rectMode(CENTER);
+        fill("yellow");
         rect(0,0,this.width ,this.height);
         pop();
     }
